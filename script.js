7700d77f24ac5523b8dd2a7ce7f6a5c6e643664b
Alan Tran <115889189+alantientran@users.noreply.github.com>
2024-10-13 23:46:39 -0500

    done

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // script.js
 
-// Alert message when the page loads
-window.onload = function() {
-  alert("Welcome to my first web page!");
-};
-
 // Function to change the text when button is clicked
-function changeText() {
-  document.getElementById("myParagraph").innerText = "You clicked the button!";
+function multiplyByThree() {
+  const inputVal = document.getElementById("assignments").value;
+  if (!isNaN(inputVal)) {
+    alert("I cried " + inputVal * 3 + " times.");
+  } else {
+    alert("Please enter a valid number.");
+  }
 }
